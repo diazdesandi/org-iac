7806ea9b394bd5dab6bb019b2a6dea885eebe6f3
rene <diaz.de.sandi.7@gmail.com>
2026-05-31 23:37:51 -0700

    refactor: add optional resource options parameter to createBranchProtection function

--- src/resources/branch.ts
+++ src/resources/branch.ts
@@ -1,8 +1,10 @@
 import github from "@pulumi/github";
+import type pulumi from "@pulumi/pulumi";
 import type { BranchProtectionArgs } from "./types";
 
 export function createBranchProtection(
 	branchArgs: BranchProtectionArgs,
+	opts?: pulumi.ResourceOptions,
 ): github.BranchProtection {
 	const { resourceName, pattern, repo, protection } = branchArgs;
 
